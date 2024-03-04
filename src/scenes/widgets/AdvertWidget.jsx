ee4f53fccff0ef615ff97c0d1794a530465fc9d6
BhanuTeja100 <bhanuteja1001@gmail.com>
2024-03-04 23:32:12 +0530

    changes in the path for deployment

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,8 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/sponser.jpg"
+        // src="http://localhost:3001/assets/sponser.jpg"
+        src="https://sociopedia-server-omega.vercel.app/assets/sponser.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
@@ -42,7 +43,8 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/tutorials.png"
+        // src="http://localhost:3001/assets/tutorials.png"
+        src="https://sociopedia-server-omega.vercel.app/assets/tutorials.png"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
@@ -64,7 +66,8 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/mamaearth.png"
+        // src="http://localhost:3001/assets/mamaearth.png"
+        src="https://sociopedia-server-omega.vercel.app/assets/mamaearth.png"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,8 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
+    // const response = await fetch("http://localhost:3001/posts", {
+    const response = await fetch("https://sociopedia-server-omega.vercel.app/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -19,7 +20,8 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
+      // `http://localhost:3001/posts/${userId}/posts`,
+      `https://sociopedia-server-omega.vercel.app/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
